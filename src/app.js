4f6e76f785e4e6fc58bbf4b0d216c9e03c0fc4db
MerajMehdizade <mdzmeraj8@gmail.com>
2023-09-14 22:13:46 +0430

    Add function to add, remove and delete inputValue

--- src/app.js
+++ src/app.js
@@ -1,26 +1,51 @@
+
+// Varibals
 let $ = document
 const todoNameInput = $.querySelector("#todoNameInput")
 const todoAddIcon = $.querySelector("#todoAddIcon")
-const todoTrashIcon = $.querySelector("#todoTrashIcon")
+const todoResetIcon = $.querySelector("#todoResetIcon")
+const todoContent = $.querySelector("#todoContent")
 let inputValue;
+todoNameInput.focus()
+// Functions
 const addNewTodo = (inputValue) => {
-    inputValue ? alert(inputValue) : alert("empty")
+  todoNameInput.classList.remove("border-danger")
+  if (inputValue) {
+    let todoBox = $.createElement("div")
+    let todoValue = $.createElement('h5')
+    let iconElm = $.createElement("i")
+    todoBox.className = "border w-30 p-4 rounded-3 text-bold pointer d-flex justify-content-between items-center"
+    iconElm.className = "bi bi-trash-fill text-danger h3 trashIcon"
+    todoValue.innerHTML = inputValue
+    todoContent.append(todoBox)
+    todoBox.append(todoValue)
+    todoBox.append(iconElm)
+    iconElm.addEventListener("click", removeTodo)
+  } else {
+    todoNameInput.classList.add("border-danger")
+  }
+
+}
+const removeTodo = (e) => {
+  e.target.parentElement.remove()
 }
 const resetInputValue = () => {
-    todoNameInput.value = ''
+  todoNameInput.value = ''
 }
-
+// EventListener
 todoNameInput.addEventListener('keyup', (event) => {
-    inputValue = event.target.value
-    if (event.keyCode === 13) {
-        addNewTodo(inputValue)
-        resetInputValue()
-    }
-})
-todoAddIcon.addEventListener("click", () => {
+  inputValue = event.target.value
+  if (event.keyCode === 13) {
     addNewTodo(inputValue)
     resetInputValue()
+    inputValue = ""
+  }
 })
-todoTrashIcon.addEventListener("click", () => {
-    resetInputValue()
+todoAddIcon.addEventListener("click", () => {
+  addNewTodo(inputValue)
+  resetInputValue()
+  inputValue = ""
+})
+todoResetIcon.addEventListener("click", () => {
+  resetInputValue()
 })
